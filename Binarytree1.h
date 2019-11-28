1ecfe4fd1ef048ca9ca3cc92ecdbf5fa081e4e47
Crossing <houshaosen2000@gmail.com>
2019-11-28 22:57:09 +0800

    modify Binarytree1.h and implement Binarytree using an linkedlist

--- Binarytree1.h
+++ Binarytree1.h
@@ -1,7 +1,7 @@
 /*
  * @Author: Crossing
  * @Date: 2019-11-27 13:25:29
- * @LastEditTime: 2019-11-28 00:46:37
+ * @LastEditTime: 2019-11-28 13:29:51
  * @Description: Implement a binary tree using an array
  * @FilePath: /src/DS/Cpp-datastructures/Binarytree1.h
  */
@@ -23,11 +23,8 @@ class Binarytree{
         void insertRoot(T x);
         void insertLeftChild(int pos, T x);
         void insertRightChild(int pos, T x);
-        int getLeftChild(int pos);
-        int getRightChild(int pos);
         int getParent(int pos);
-        void deleteLeftChild(int pos);
-        void deleteRightChild(int pos);
+        void deleteNode(int pos);
         void preordertraverse(int i);
         void midordertraverse(int i);
         void postordertraverse(int i);
@@ -83,14 +80,6 @@ void Binarytree<T>::insertRightChild(int pos, T x){
     count++;
 }
 template <class T>
-int Binarytree<T>::getLeftChild(int pos){
-    return t[2*pos+1];
-}
-template <class T>
-int Binarytree<T>::getRightChild(int pos){
-    return t[2*pos+2];
-}
-template <class T>
 int Binarytree<T>::getParent(int pos){
     if(pos == 0) {
         cout<<"Root has no parent"<<endl;
@@ -99,18 +88,13 @@ int Binarytree<T>::getParent(int pos){
     else return t[(pos - 1)/2];
 }
 template <class T>
-void Binarytree<T>::deleteLeftChild(int pos){
-    t[2*pos+1] = 0;
-    count--;
-    if(getLeftChild(2*pos+1))   deleteLeftChild(2*pos+1);
-    if(getRightChild(2*pos+1))   deleteRightChild(2*pos+1);
-}
-template <class T>
-void Binarytree<T>::deleteRightChild(int pos){
-    t[2*pos+2] = 0;
-    count--;
-    if(getLeftChild(2*pos+2))   deleteLeftChild(2*pos+2);
-    if(getRightChild(2*pos+2))  deleteRightChild(2*pos+2);
+void Binarytree<T>::deleteNode(int pos){
+    if(getValue(pos)){
+        deleteNode(2*pos+1);
+        deleteNode(2*pos+2);
+        t[pos] = 0;
+        count--;
+    }
 }
 template <class T>
 void Binarytree<T>::preordertraverse(int i){
---
+++ Binarytree2.h
@@ -0,0 +1,130 @@
+/*
+ * @Author: Crossing
+ * @Date: 2019-11-28 01:02:10
+ * @LastEditTime: 2019-11-28 22:56:03
+ * @Description: Implement a binary tree using a linkedlist
+ * @FilePath: /src/DS/Cpp-datastructures/Binarytree2.h
+ */
+#ifndef BINARYTRREE_H
+#define BINARYTRREE_H
+
+#include<iostream>
+using namespace std;
+template <class T>
+class Node{
+    public:
+        Node(T val, Node* l, Node* r):value(val), lchild(l), rchild(r){}
+        Node* lchild;
+        Node* rchild;
+        T value;
+};
+template <class T>
+class Binarytree{
+    public:
+        Binarytree();
+        ~Binarytree();
+        void destroyNode(Node<T>*& n);
+        T getValue(Node<T>* n);
+        Node<T>* getRoot();
+        int getDepth(Node<T>* n);
+        bool isEmpty();
+        void insertRoot(T x);
+        void insertLeftChild(Node<T>* n, T x);
+        void insertRightChild(Node<T>* n, T x);
+        Node<T>* getParent(Node<T>* s, Node<T>* n);
+        void preordertraverse(Node<T>* n);
+        void midordertraverse(Node<T>* n);
+        void postordertraverse(Node<T>* n);
+    private:
+        Node<T>* root;
+};
+template <class T>
+Binarytree<T>::Binarytree(){
+    root = nullptr;
+}
+template <class T>
+Binarytree<T>::~Binarytree(){
+    destroyNode(root);
+}
+template <class T>
+void Binarytree<T>::destroyNode(Node<T>*& n){
+    if(n){
+        destroyNode(n->lchild);
+        destroyNode(n->rchild);
+        delete n;
+        n = nullptr;
+    }
+}
+template <class T>
+T Binarytree<T>::getValue(Node<T>* n){
+    if(n != nullptr) return n->value;
+    else {
+        cout<<"nullptr"<<endl;
+        return root->value;
+    }
+}
+template <class T>
+int Binarytree<T>::getDepth(Node<T>* n){
+    if(n != nullptr){
+        int i = getDepth(n->lchild);
+        int j = getDepth(n->rchild);
+        return i < j ? j+1 : i+1;
+    }
+    return 0;
+}
+template <class T>
+bool Binarytree<T>::isEmpty(){
+    return (root == nullptr);
+}
+template <class T>
+void Binarytree<T>::insertRoot(T x){
+    root = new Node<T>(x, nullptr, nullptr);
+}
+template <class T>
+void Binarytree<T>::insertLeftChild(Node<T>* n, T x){
+    Node<T>* newNode = new Node<T>(x, nullptr, nullptr);
+    n->lchild = newNode;
+}
+template <class T>
+void Binarytree<T>::insertRightChild(Node<T>* n, T x){
+    Node<T>* newNode = new Node<T>(x, nullptr, nullptr);
+    n->rchild = newNode;
+}
+//I can't implement it on my own so I learn it from someone on the Internet
+template <class T>
+Node<T>* Binarytree<T>::getParent(Node<T>* s, Node<T>* n){
+    if(s == nullptr) return nullptr;
+    if(s->lchild == n || s->rchild == n) return s;
+    Node<T>* p = getParent(s->lchild, n);
+    if(p) return p;
+    return getParent(s->rchild, n);
+}
+template <class T>
+void Binarytree<T>::preordertraverse(Node<T>* n){
+    if(n){
+        cout<<n->value;
+        preordertraverse(n->lchild);
+        preordertraverse(n->rchild);
+    }
+}
+template <class T>
+void Binarytree<T>::midordertraverse(Node<T>* n){
+    if(n){
+        midordertraverse(n->lchild);
+        cout<<n->value;
+        midordertraverse(n->rchild);
+    }
+}
+template <class T>
+void Binarytree<T>::postordertraverse(Node<T>* n){
+    if(n){
+        postordertraverse(n->lchild);
+        postordertraverse(n->rchild);
+        cout<<n->value;
+    }
+}
+template <class T>
+Node<T>* Binarytree<T>::getRoot(){
+    return root;
+}
+#endif
