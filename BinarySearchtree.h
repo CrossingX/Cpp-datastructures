0153c6e2b5bf7c1b84868e513f08e997390dc34e
Crossing <houshaosen2000@gmail.com>
2019-12-07 16:41:13 +0800

    modify deleteNode in BinarySearchtree

--- BinarySearchtree.h
+++ BinarySearchtree.h
@@ -1,7 +1,7 @@
 /*
  * @Author: Crossing
  * @Date: 2019-12-06 23:46:19
- * @LastEditTime: 2019-12-07 15:28:15
+ * @LastEditTime: 2019-12-07 16:40:30
  * @Description: Implement a binary search tree using a linkedlist
  * @FilePath: /src/DS/Cpp-datastructures/BinarySearchtree.h
  */
@@ -26,9 +26,9 @@ class Binarysearchtree{
         Binarysearchtree();
         ~Binarysearchtree();
         void insertNode(T x, Node<T>* a);   // a should be root
-        void deleteNode(T x, Node<T>* a);   // a should be root
-        Node<T>* findMax();
-        Node<T>* findMin();
+        Node<T>* deleteNode(T x, Node<T>* a);   // a should be root
+        Node<T>* findMax(Node<T>* a);
+        Node<T>* findMin(Node<T>* a);
         Node<T>* find(T x);     // a should be root
         void destroyNode(Node<T>*& a);           // a should be root
         void preordertraverse(Node<T>* a);
@@ -68,31 +68,58 @@ void Binarysearchtree<T>::insertNode(T x, Node<T>* a){
         }
     }
 }
+//Thanks Qinming He from http://www.icourse163.org/course/ZJU-93001?tid=1001757011 for help 
 template <class T>
-void Binarysearchtree<T>::deleteNode(T x, Node<T>* a){
-    if(a == nullptr) cout<<"Can't find it."<<endl;
-    else{
-        if(a->value > x) { 
-            if(a->lchild != nullptr){
-                if(a->lchild->value == x){
-                    destroyNode(a->lchild);
-                    a->lchild = nullptr;
-                }
-                else deleteNode(x,a->lchild);
-            }
-        }
-        else if(a->value < x) {
-            if(a->rchild != nullptr){
-                if(a->rchild->value == x){
-                    destroyNode(a->rchild);
-                    a->rchild = nullptr;
-                }
-                else deleteNode(x,a->rchild);
-            }
-        }
-        else if(a->value == x) destroyNode(a);
+Node<T>* Binarysearchtree<T>::deleteNode(T x, Node<T>* a){ 
+    // if(a == nullptr) cout<<"Can't find it."<<endl;
+    // else{
+    //     if(a->value > x) {
+    //         if(a->lchild != nullptr){
+    //             if(a->lchild->value == x){
+    //                 destroyNode(a->lchild);
+    //                 a->lchild = nullptr;
+    //             }
+    //             else deleteNode(x,a->lchild);
+    //         }
+    //     }
+    //     else if(a->value < x) {
+    //         if(a->rchild != nullptr){
+    //             if(a->rchild->value == x){
+    //                 destroyNode(a->rchild);
+    //                 a->rchild = nullptr;
+    //             }
+    //             else deleteNode(x,a->rchild);
+    //         }
+    //     }
+    //     else if(a->value == x) destroyNode(a);
+    // }
+    if(a == nullptr) return nullptr;
+    if(a->value > x) {
+        a->lchild = deleteNode(x, a->lchild);
     }
+    else if(a->value < x) {
+        a->rchild = deleteNode(x, a->rchild);
+    }
+    else{
+        if(a->lchild != nullptr && a->rchild != nullptr){
+            Node<T>* tmp = findMin(a->rchild);
+            a->value = tmp->value;
+            a->rchild = deleteNode(a->value, a->rchild);
+        }
+        else {
+            Node<T>* tmp = a;
+            if(a->lchild == nullptr){
+                a = a->rchild;
+            }
+            else if(a->rchild == nullptr){
+                a = a->lchild;
+            }
+            delete tmp;
+        }
+    }
+    return a;
 }
+
 template <class T>
 void Binarysearchtree<T>::destroyNode(Node<T>*& a){
     if(a != nullptr){
@@ -103,18 +130,18 @@ void Binarysearchtree<T>::destroyNode(Node<T>*& a){
     }
 }
 template <class T>
-Node<T>* Binarysearchtree<T>::findMax(){
-    if(root == nullptr) return nullptr;
-    Node<T>* p = root;
+Node<T>* Binarysearchtree<T>::findMax(Node<T>* a){
+    if(a == nullptr) return nullptr;
+    Node<T>* p = a;
     while(p->rchild != nullptr){
         p = p->rchild;
     }
     return p;
 }
 template <class T>
-Node<T>* Binarysearchtree<T>::findMin(){
-    if(root == nullptr) return nullptr;
-    Node<T>* p = root;
+Node<T>* Binarysearchtree<T>::findMin(Node<T>* a){
+    if(a == nullptr) return nullptr;
+    Node<T>* p = a;
     while(p->lchild != nullptr){
         p = p->lchild;
     }
